ef6328fd6eeb0bb9ad149dd815734d7b499cb839
Paul Malagnoux <paulmalagnoux26@gmail.com>
2023-05-31 15:21:33 +0200

    feat(front-header Landing page) : Création du componant landing page et création de la barre de Header

--- front/organizer-front/src/app/app.module.ts
+++ front/organizer-front/src/app/app.module.ts
@@ -3,16 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactService } from './service/contact.service';
+import { FileService } from './service/file.service';
+import { PerimeterService } from './service/perimeter.service';
+import { TagService } from './service/tag.service';
+import { TypeService } from './service/type.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+
   ],
-  providers: [],
+  providers: [
+    ContactService,
+    FileService,
+    PerimeterService,
+    TagService,
+    TypeService
+  
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  
+ }
