219d2c28457f9902b56bd73c91bdc36746515d36
Paul Malagnoux <paulmalagnoux26@gmail.com>
2023-06-02 10:29:39 +0200

    feat(api) : Ajout de la gestion des CORS

--- api/src/main/java/com/solutec/api/OrganizerApplication.java
+++ api/src/main/java/com/solutec/api/OrganizerApplication.java
@@ -2,10 +2,10 @@ package com.solutec.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.web.cors.CorsConfiguration;
-// import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-// import org.springframework.web.filter.CorsFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class OrganizerApplication {
@@ -14,21 +14,18 @@ public class OrganizerApplication {
 		SpringApplication.run(OrganizerApplication.class, args);
 	}
 
-	// @Bean
-	// public CorsFilter corsFilter() {
-	// UrlBasedCorsConfigurationSource source = new
-	// UrlBasedCorsConfigurationSource();
-	// CorsConfiguration config = new CorsConfiguration();
-	// config.setAllowCredentials(true);
-	// config.addAllowedOrigin("*");
-	// config.addAllowedHeader("*");
-	// config.addAllowedMethod("OPTIONS");
-	// config.addAllowedMethod("GET");
-	// config.addAllowedMethod("POST");
-	// config.addAllowedMethod("PUT");
-	// config.addAllowedMethod("DELETE");
-	// config.setMaxAge(3600L);
-	// source.registerCorsConfiguration("/**", config);
-	// return new CorsFilter(source);
-	// }
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("DELETE");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
 }
